78fc335a7f26d099baa2100d43e9a3515256a3aa
zhangxs7 <zhangxs7@lenovo.com>
2017-07-04 17:52:15 +0800

    修改 基本网页

--- web-webapp/src/main/java/com/yisun/web/controller/ToOrderController.java
+++ web-webapp/src/main/java/com/yisun/web/controller/ToOrderController.java
@@ -1,15 +1,10 @@
 package com.yisun.web.controller;
 
 import com.yisun.web.common.MD5;
-import com.yisun.web.common.util.JacksonUtil;
-import com.yisun.web.common.util.ResponseData;
-import com.yisun.web.domain.RemoteResult;
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -69,128 +64,6 @@ public class ToOrderController {
         request.setAttribute("payStatus", payStatus);
         return "pay/queryOP";
     }
-
-    @RequestMapping("order/queryOrderToPay")
-    public String queryOrderToPay(HttpServletRequest request, HttpServletResponse response) {
-        return "pay/queryOrderToPay";
-    }
-
-    @RequestMapping("order/queryOrderToPayQuery")
-    public String queryOrderToPayQuery(HttpServletRequest request, HttpServletResponse response) {
-        ResponseData responseData=new ResponseData();
-        String orderCode=request.getParameter("orderCode");
-        String lenovoId=request.getParameter("lenovoId");
-        String shopId=request.getParameter("shopId");
-        String terminal=request.getParameter("terminal");
-        if(StringUtils.isBlank(orderCode) || !orderCode.matches("^[0-9]*$")){
-            responseData.setSuccess(false);
-            responseData.setMsg("订单错误！");
-            return JacksonUtil.toJson(responseData);
-        }
-        if(StringUtils.isBlank(lenovoId) || !lenovoId.matches("^[0-9]*$")){
-            responseData.setSuccess(false);
-            responseData.setMsg("lenovoId错误！");
-            return JacksonUtil.toJson(responseData);
-        }
-        if(StringUtils.isBlank(shopId) || !shopId.matches("^[0-9]*$")){
-            responseData.setSuccess(false);
-            responseData.setMsg("shopId错误！");
-            return JacksonUtil.toJson(responseData);
-        }
-        if(StringUtils.isBlank(terminal) || !terminal.matches("^[0-9]*$")){
-            responseData.setSuccess(false);
-            responseData.setMsg("terminal错误！");
-            return JacksonUtil.toJson(responseData);
-        }
-
-//        List<String> orderCodeList=new ArrayList<String>();
-//        orderCodeList.add(orderCode);
-//        com.yisun.web.domain.RemoteResult<List<OrderVo>> rs = null;
-//        rs= payMainService.queryOrderMainDetail(orderCodeList,shopId,terminal,lenovoId);
-        return "";
-    }
-
-    @RequestMapping("order/queryOPData")
-    @ResponseBody
-    public String queryOPData(HttpServletRequest request, HttpServletResponse response,Map formData) {
-        ResponseData responseData=new ResponseData();
-        String signkeyPage=request.getParameter("signkey");
-        if(StringUtils.isBlank(signkeyPage)){
-            return "403";
-        }
-        SimpleDateFormat dateFormat=new SimpleDateFormat("yyyyMM");
-        String day=dateFormat.format(new Date());
-        Map<String,Object> keyMap=new HashMap<String, Object>();
-        keyMap.put("signKeyDb",signKeyDb);
-        keyMap.put("day",day);
-        String getKey=getSign(keyMap,signKey);
-        if(!getKey.equals(signkeyPage)){
-            responseData.setMsg("key err！");
-            return JacksonUtil.toJson(responseData);
-        }
-        String orderCode=request.getParameter("orderCode");
-        String mainFlag=request.getParameter("mainFlag");
-        String transactionId=request.getParameter("transactionId");
-        String payType=request.getParameter("payType");
-        String payTime=request.getParameter("payTime");
-        Map map=new HashMap();
-        if(StringUtils.isBlank(orderCode) || !orderCode.matches("^[0-9]*$")){
-            responseData.setSuccess(false);
-            responseData.setMsg("订单错误！");
-            return JacksonUtil.toJson(responseData);
-        }else {
-            map.put("orderCode",orderCode.trim());
-        }
-
-        if(StringUtils.isNotBlank(mainFlag)&&mainFlag.matches("^[01]?")){
-            map.put("mainFlag", mainFlag);
-        }else {
-            responseData.setSuccess(false);
-            responseData.setMsg("订单标识错误！");
-            return JacksonUtil.toJson(responseData);
-        }
-
-        if(StringUtils.isNotBlank(transactionId) && transactionId.matches("^[0-9]*$")){
-            map.put("transactionId",transactionId.trim());
-        }else {
-            responseData.setSuccess(false);
-            responseData.setMsg("流水错误！");
-            return JacksonUtil.toJson(responseData);
-        }
-
-        if(StringUtils.isNotBlank(payType)&& payType.matches("^[0-9]*$")){
-            map.put("payType",payType.trim());
-        }else {
-            responseData.setSuccess(false);
-            responseData.setMsg("支付方式错误！");
-            return JacksonUtil.toJson(responseData);
-        }
-
-        if(StringUtils.isNotBlank(payTime)&& payTime.matches("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}")){
-            map.put("payTime",payTime.trim());
-        }else {
-            responseData.setSuccess(false);
-            responseData.setMsg("支付时间错误！");
-            return JacksonUtil.toJson(responseData);
-        }
-        try {
-//            RemoteResult<Integer> result=paySubService.updateChannelOrder(map);
-            RemoteResult<Integer> result=new RemoteResult<Integer>(false);
-            if(result.isSuccess()){
-                logger.info("修改成功 参数"+map);
-                responseData.setMsg(result.getResultMsg());
-            }else {
-                logger.info("修改失败 参数"+map+"  错误原因："+result.getResultMsg());
-                responseData.setMsg(result.getResultMsg());
-            }
-        }catch (Exception e){
-            logger.info("修改失败 参数"+map+"  错误原因："+e.getMessage());
-            responseData.setMsg(e.getMessage());
-        }
-
-        return JacksonUtil.toJson(responseData);
-    }
-
 
     /**
      * 获取验签
